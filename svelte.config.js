8c0dc53013dd2f29716a59e2dd9b34f7e69b2d65
Sofia N. Lima <me@dzshn.xyz>
2022-06-03 23:57:06 -0300

    thanks rollup

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,31 @@
 import adapter from "@sveltejs/adapter-static";
+import path from "path";
 
 export default {
     kit: {
         adapter: adapter(),
         prerender: {
             default: true,
+        },
+        vite: {
+            build: {
+                rollupOptions: {
+                    plugins: [
+                        {
+                            // 🥰
+                            resolveImportMeta(property, { moduleId }) {
+                                if (property === "url") {
+                                    return `new URL('${path.relative(
+                                        process.cwd(),
+                                        moduleId
+                                    )}', document.baseURI).href`;
+                                }
+                                return null;
+                            },
+                        },
+                    ],
+                },
+            },
         },
     },
 };
